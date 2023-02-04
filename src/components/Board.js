c9c98497cb2993eaf043191633e81365c7fa00e5
cybitalex <alex@LaMimi>
2023-02-04 16:12:54 -0500

    added difficulties

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,28 +2,48 @@ import React,{useState,useEffect} from "react";
 import CreateBoard from '../utils/CreateBoard';
 import Cell from "./Cell";
 import { revealed } from "../utils/Reveal";
+import Dropdown from 'react-bootstrap/Dropdown';
+import { click } from "@testing-library/user-event/dist/click";
+
 
 function Board() {
     const [grid,setGrid]=useState([]);
     const [nonMinecount,setNonMinecount]=useState(0);
     const [mineLocation,setmineLocation]=useState([]);
+    const [value1, setValue1] = useState(0);
+    const [value2, setValue2] = useState(0);
+    const [value3, setValue3] = useState(0);
+    // const [freshboard, setFreshboard] = useState(() => {
+    //     const freshBoard = (num1, num2, num3) => {
+    //         const newBoard=CreateBoard(num1,num2,num3);
+    //         setNonMinecount(num1*num2-num3);
+    //         setmineLocation(newBoard.mineLocation);
+    //         setGrid(newBoard.board);
+    //     }
+    //     freshBoard(10, 10, 20)
+    // })
     const style={
             display : 'flex',
             flexDirection : 'row',
             width:'fit-content',
             color:'white',
             
-        }    
+        }
+        
 
     useEffect(()=>{
-        const freshBoard = () => {
-            const newBoard=CreateBoard(10,10,20);
-            setNonMinecount(10*10-20);
-            setmineLocation(newBoard.mineLocation);
-            setGrid(newBoard.board);
-        }
-        freshBoard();
+        return freshBoard(10, 10, 20);
     },[]);
+
+    let clickedOption = []
+
+    const freshBoard = (num1, num2, num3) => {
+        const newBoard=CreateBoard(num1,num2,num3);
+        setNonMinecount(num1*num2-num3);
+        setmineLocation(newBoard.mineLocation);
+        setGrid(newBoard.board);
+    }
+    
     
     const updateFlag=(e,x,y)=>{
         e.preventDefault();
@@ -55,14 +75,31 @@ function Board() {
         <div className="parent">
             <div style={{color:"white",textAlign:"center",fontSize:"35px"}}>Non-Mines : {nonMinecount}</div>
             <div>
-                
+               <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Dropdown Button
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => {
+                            freshBoard(4, 4, 8)
+                        }}>Board of 4</Dropdown.Item>
+                        <Dropdown.Item onClick={() => {
+                            freshBoard(8, 8, 16)
+                        }}>Board of 8</Dropdown.Item>
+                        <Dropdown.Item onClick={() => {
+                            freshBoard(10, 10, 20)
+                        }}>Board of 10</Dropdown.Item>
+                        {/* <Dropdown.Item onClick={setValues([8, 8, 16])}>Board of 8</Dropdown.Item> */}
+                    </Dropdown.Menu>
+                </Dropdown> 
                 {grid.map((singlerow,index1)=>{
                     return (
-                        <div style={style} key={index1}>
-                            {singlerow.map((singlecol,index2)=>{
-                            return  <Cell details={singlecol} key={index2} updateFlag={updateFlag} revealcell={revealcell}/>
-                            })}
-                        </div>
+                            <div style={style} key={index1}>
+                                {singlerow.map((singlecol,index2)=>{
+                                return  <Cell details={singlecol} key={index2} updateFlag={updateFlag} revealcell={revealcell}/>
+                                })}
+                            </div>
                     )
                 })}
             </div>
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,22 +2,44 @@ import React from 'react'
 
 export default function Cell({details,updateFlag,revealcell}) {
     const style={
-        cellStyle:{
-            width:40,height:40,backgroundColor:'grey',border:'1px solid white',
+       cellStyle:{
+            width:40,
+            height:40,
+            backgroundColor:details.revealed && details.value!==0?details.value==='X'?'red':' #00226d':details.revealed&&details.value===0?'#00226f':'#000',
+            opacity:'0.8',
+            border:'3px solid white',
             display:'flex',
             justifyContent:'center',
             alignItems:'center',
             fontSize:'20px',
-            cursor: 'pointer'
+            cursor:'pointer',
+            color:'cyan',
+            fontWeight:'1000' 
         },
     }
-    const handleClick=()=>{
-        console.log(details);
+
+    const click=()=>{
+        // calling revealcell for specific cell x and y
+        revealcell(details.x,details.y);  
+    }
+
+    const rightclick=(e)=>{
+        updateFlag(e,details.x,details.y)
     }
     
     return (
-        <div style={style.cellStyle} onClick={()=>{revealcell(details.x,details.y)}} onContextMenu={(e)=>updateFlag(e,details.x,details.y)}>
-            {details.revealed ? details.value : ""}
+         <div style={style.cellStyle} onClick={click} onContextMenu={rightclick}>
+            {!details.revealed && details.flagged ? (
+        "🚩"
+      ) : details.revealed && details.value !== 0 ? (
+        details.value === "X" ? (
+          "💣"
+        ) : (
+          details.value
+        )
+      ) : (
+        ""
+      )}
         </div>
     )
 }
